b947eb028fda5346d5940f7b1a8740b997852fa8
iftekharemon703 <iftekharemon703@gmail.com>
2021-02-01 23:13:58 +0600

    null vs undifined

---
+++ null-vs-undifined.js
@@ -0,0 +1,25 @@
+undifined 
+
+let emon;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return; // if return value not set this is undifine
+            // return not set is also undifine
+}
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+const result = add(12); // perametar value not set is undifined
+console.log(result);
+
+const student = {name: 'iftekahr', roll: 44};
+console.log(student.gf); // not set property is undifined
+
+let fun = undifined;
+console.log(fun);
+
+
+null
+
+null always set null
