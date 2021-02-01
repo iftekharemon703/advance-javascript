dd374eb9499d46acc622d590cec9308512358309
iftekharemon703 <iftekharemon703@gmail.com>
2021-02-01 23:27:46 +0600

    dubble vs triple equal

---
+++ dubble-vs-triple-equal.js
@@ -0,0 +1,19 @@
+// const first = 3;
+// const second = "3";
+// if(first === second){
+//     console.log('condition is true');
+// }else{
+//     console.log('condition is false')
+// }
+
+// dubble equal only check number, triple equal checked number + type
+
+const first = 1;
+const second = false;
+if(first === second){
+    console.log('condition is true');
+}else{
+    console.log('condition is false')
+}
+
+// javascript rule 0 is false and 1 is true
