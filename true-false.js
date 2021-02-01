76efb9ddde80c3b5bd3fb7e1ee6f8f844627ae59
iftekharemon703 <iftekharemon703@gmail.com>
2021-02-01 22:51:53 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// falsey
+// false
+// 0
+// ""(empty string)
+// undifine 
+// NULL
+// NaN 
+
+// truethy
+// ' ' white space string a true
+// [] empty array is true
+// {} empty object is true
+// '0' all string is true
+// 'false' is true
+
+
